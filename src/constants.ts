e8a67b0ebb207612a6d3e3564422687d3268cdc3
Cesar Mayora <cesarmay@protonmail.com>
2023-08-24 19:46:03 +0200

    export constant FC env variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,11 @@
+import { load } from "std/dotenv/mod.ts";
+
+const ENV = await load();
+
+export const FC_BASE_URL = ENV["FC_BASE_URL"];
+export const FC_API_VERSION = ENV["FC_API_VERSION"];
+export const FC_API_KEY = ENV["FC_API_KEY"];
+
 // TODO: put a list of all currencies available
 export const CURRENCY_CODES: string[] = [
   "EUR",
