7e2e35833a09a80c6e83ba84561595ce50918791
Cesar Mayora <cesarmay@protonmail.com>
2023-08-25 19:14:27 +0200

    `.env` file loading good

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { fetchAvailableCurrencies } from "./api.ts";
 import { loadEnvs } from "./lib/env.ts";
 
-export const ENV = loadEnvs();
+export const ENV = await loadEnvs();
 
 export const FC_BASE_URL = ENV["FC_BASE_URL"];
 export const FC_API_VERSION = ENV["FC_API_VERSION"];
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,24 +1,32 @@
+import { load } from "https://deno.land/std@0.200.0/dotenv/mod.ts";
+
 export const DEFAULT_FC_BASE_URL = "https://api.freecurrencyapi.com";
 export const DEFAULT_FC_API_VERSION = "v1";
 
-export function loadEnvs() {
-  const ENV: Record<string, string> = {
-    FC_BASE_URL: DEFAULT_FC_BASE_URL,
-    FC_API_VERSION: DEFAULT_FC_API_VERSION,
-  };
+export async function loadEnvs() {
+  try {
+    const ENV = await load();
 
-  // set the API key
-  if (
-    Deno.env.has("FC_API_KEY") && Deno.env.get("FC_API_KEY") !== undefined &&
-    Deno.env.get("FC_API_KEY") !== ""
-  ) {
-    ENV["FC_API_KEY"] = Deno.env.get("FC_API_KEY")!;
-  } else {
-    const message =
-      "FC_API_KEY have not been found.\nPlease provide an API key exporting an FC_API_KEY environment variable.\nFollow instructions on https://github.com/mayoras/conv#api-usage for further information.";
-    console.error(message);
-    Deno.exit(1);
+    return ENV;
+  } catch (_err) {
+    const ENV: Record<string, string> = {
+      FC_BASE_URL: DEFAULT_FC_BASE_URL,
+      FC_API_VERSION: DEFAULT_FC_API_VERSION,
+    };
+
+    // set the API key
+    if (
+      Deno.env.has("FC_API_KEY") && Deno.env.get("FC_API_KEY") !== undefined &&
+      Deno.env.get("FC_API_KEY") !== ""
+    ) {
+      ENV["FC_API_KEY"] = Deno.env.get("FC_API_KEY")!;
+    } else {
+      const message =
+        "FC_API_KEY have not been found.\nPlease provide an API key exporting an FC_API_KEY environment variable.\nFollow instructions on https://github.com/mayoras/conv#api-usage for further information.";
+      console.error(message);
+      Deno.exit(1);
+    }
+
+    return ENV;
   }
-
-  return ENV;
 }
