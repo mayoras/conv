ef8646b161e07e66b6e9dec237a5e152694a2fef
Cesar Mayora <cesarmay@protonmail.com>
2023-08-26 15:52:23 +0200

    log warning messasge

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -1,7 +1,13 @@
+import chalk from "npm:chalk";
+
 export async function waitForEnter(): Promise<string> {
   const charBuffer = new Uint8Array(1);
 
   await Deno.stdin.read(charBuffer);
 
   return new TextDecoder().decode(charBuffer);
+}
+
+export function logWarning(msg: string): void {
+  console.warn(chalk.yellow.bold("WARNING: ") + msg);
 }
