a6048c808b463be26dc0223ad8edfc43d490b036
Cesar Mayora <cesarmay@protonmail.com>
2023-08-26 15:08:39 +0200

    list all currencies command

---
+++ src/lib/commands/list.ts
@@ -0,0 +1,23 @@
+import { fetchAvailableCurrencies } from "../../api.ts";
+
+const NUM_COLUMNS = 3;
+
+export async function listAllCurrencies() {
+  const currencies = await fetchAvailableCurrencies();
+
+  if (currencies === null) {
+    throw new Error("Could not fetch currencies.");
+  }
+
+  const numRows = Math.ceil(currencies.length / NUM_COLUMNS);
+
+  for (let i = 0; i < numRows; i++) {
+    let row = "";
+    for (let j = i; j < currencies.length; j += numRows) {
+      row += currencies[j].padEnd(15, " ");
+    }
+    console.log(row);
+  }
+
+  Deno.exit(0);
+}
