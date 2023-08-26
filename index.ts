ea66b74c39cb494e95b9431d748b0d5a24ad2281
Cesar Mayora <cesarmay@protonmail.com>
2023-08-26 18:34:47 +0200

    exit program using cli opts

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import * as _constants from "./src/constants.ts";
 import { main } from "./src/main.ts";
 
-main(Deno.args);
+try {
+  await main(Deno.args);
+  Deno.exit(0);
+} catch (_err) {
+  Deno.exit(1);
+}
